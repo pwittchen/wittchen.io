a94b2285cc03e69513c9491edef1cabab2631cb3
Piotr Wittchen <piotr@wittchen.biz.pl>
2018-02-20 22:57:03 +0100

    Update gulpfile.js

--- themes/pwittchen/gulpfile.js
+++ themes/pwittchen/gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass 
 gulp.task('sass', function() {
     return gulp.src('./source/scss/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
@@ -10,7 +9,6 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('./source/css'));
 });
 
-// 实时编译
 gulp.task('default', ['sass'], function() {
     gulp.watch('./source/scss/_partial/*.scss', ['sass']);
     gulp.watch('./source/scss/*.scss', ['sass']);
