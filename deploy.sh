b2c9b9aa0e64fd2bf06eeec74c8933ba60e1e794
Piotr Wittchen <piotr@wittchen.io>
2019-04-10 20:26:28 +0200

    updating deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
 source .ftpconfig
-echo "starting deployment process"
 lftp ftp://$USER:$PASS@$HOST -e "set ftp:ssl-allow no; mirror -R -v --only-newer public domains/wittchen.io/public_html; quit"
-echo "deployment process finished"
