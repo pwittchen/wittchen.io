b5aa6e6ddaa8a0d19dffb7a575bc5795166ddb9c
Piotr Wittchen <piotr@wittchen.io>
2019-10-24 22:19:02 +0200

    updating deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 source .ftpconfig
-
 lftp ftp://$USER:$PASS@$HOST -e "set ftp:ssl-allow no; rm domains/$HOST/public_html/css/apollo.css; mirror -R -v --only-newer public domains/$HOST/public_html; quit"
